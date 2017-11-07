8e931497c029777fad48289ffb9b4941e172f1d0
Meera <mjvsharma@outlook.com>
2017-11-07 12:38:48 -0500

    wrote pick up passengers function

--- autopilot.js
+++ autopilot.js
@@ -4,10 +4,12 @@ function getNewCar() {
     passengers: 0,
     gas: 100
   }
+  return car;
 }
 
 
 var cars = [];
+
 function addCar() {
   var new_car = getNewCar();
   cars.push(new_car)
@@ -18,23 +20,33 @@ function addCar() {
 
 
 
-// function pickUpPassenger(car) {
-//
-//
-// }
 
-// def pick_up_passenger(car)
-//   car[:passengers] += 1
-//   car[:gas] -= 10
-//   "Picked up passenger. Car now has #{car[:passengers]} passengers."
-// end
-//
-// def get_destination(car)
-//   if car[:city] == 'Toronto'
-//     'Mississauga'
-//   elsif car[:city] == 'Mississauga'
-//     'London'
-//   elsif car[:city] == 'London'
-//     'Toronto'
-//   end
-// end
+function pickUpPassenger() {
+  car = getNewCar();
+
+  car["passengers"] +=1;
+  car["gas"] -=10;
+
+"Picked up passenger. Car now has #{car[:passengers]} passengers."
+  console.log("Picked up passenger. Car now has " + car["passengers"] + " passengers.");
+
+}
+
+pickUpPassenger();
+
+
+def get_destination(car)
+  if car[:city] == 'Toronto'
+    'Mississauga'
+  elsif car[:city] == 'Mississauga'
+    'London'
+  elsif car[:city] == 'London'
+    'Toronto'
+  end
+end
+
+def fill_up_gas(car)
+  old_gas = car[:gas]
+  car[:gas] = 100
+  "Filled up to #{ get_gas_display(car[:gas]) } on gas from #{ get_gas_display(old_gas) }."
+end
